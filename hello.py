b1f9251fff37daf8d79c0153f34efbb6f966ce95
Tenlucena <samuellucenasantos@gmail.com>
2025-09-05 21:22:23 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,24 +1,21 @@
-from datetime import datetime
-from flask import Flask, render_template, request, make_response, redirect, abort
+from flask import Flask, render_template, request, session, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'uma_chave_secreta_segura_aqui'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-from flask import Flask, render_template, session, redirect, url_for, flash
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -26,33 +23,32 @@ def index():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
-        session['name'] = form.name.data;
-        return redirect(url_for('index'));
-     return render_template('index.html', form=form, name=session.get('name'))
-
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>/<prontuario>/<institution>')
 def user(name, prontuario, institution):
     return render_template('user.html', 
                            name=name, 
                            prontuario=prontuario,
-                           institution=institution);
+                           institution=institution)
 
 @app.route('/contextorequisicao/<name>')
 def contextorequisicao(name):
-    user_agent = request.headers.get('User-Agent');
-    remote_addr = request.remote_addr;
-    remote_host = request.host;
+    user_agent = request.headers.get('User-Agent')
+    remote_addr = request.remote_addr
+    remote_host = request.host
     return render_template('contexto.html',
                            name=name, 
                            user_agent=user_agent, 
                            remote_addr=remote_addr,
-                           remote_host=remote_host);
+                           remote_host=remote_host)
 
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
