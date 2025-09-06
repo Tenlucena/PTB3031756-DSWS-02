a9e1bdc341e161cf09c1a668f3f61d78f407cbe9
Tenlucena <samuellucenasantos@gmail.com>
2025-09-05 21:20:23 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -18,10 +18,17 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
-@app.route('/')
+from flask import Flask, render_template, session, redirect, url_for, flash
+@app.route('/', methods=['GET', 'POST'])
 def index():
-     return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data;
+        return redirect(url_for('index'));
+     return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>/<prontuario>/<institution>')
